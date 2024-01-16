3497315d95d36dcd5707ee1524dea1eb23611e94
Sarthak Gupta <118627778+sarthakgupta096@users.noreply.github.com>
2024-01-16 07:26:10 +0000

    type converstion

---
+++ basic/data types converstion.js
@@ -0,0 +1,63 @@
+let score = null;
+
+console.log(typeof score);
+console.log(typeof score);
+
+// convert into number
+let value = Number(score);
+console.log(typeof value);
+console.log(value);
+
+/*
+   "33"==>33
+   "5df"==>nan when we try to convert in number.
+*/
+
+// number to boolean
+let b = 1;
+let c = Boolean(b);
+console.log(c);
+
+// number to string
+let x = 33;
+let st = String(x);
+console.log(st);
+console.log(typeof st);
+
+// ******************************** Operations ***************************************
+
+//convert into negative value
+let val = 33;
+let neg = -val;
+console.log(neg);
+
+//power
+
+console.log(2 ** 3);
+
+//string very imp concept
+
+let str1 = "sar";
+let str2 = "sur";
+
+// console.log(str1 + str2);
+
+// console.log("1" + 2);
+
+// console.log("1" + 2 + 2);
+// console.log(2 + 5 + "5");
+
+console.log(+true)//1
+console.log(+"")//0
+
+let z=100
+z++
+console.log(z); 
+
+let m=200
+let k=m++
+console.log(k);
+
+
+
+
