19786a9e198de21fe82f88b791afbf33353857ba
Sarthak Gupta <118627778+sarthakgupta096@users.noreply.github.com>
2024-01-16 07:20:53 +0000

    Variables

---
+++ basic/Variable.js
@@ -0,0 +1,29 @@
+
+console.log("sarthak");
+console.log("surbhi");
+let a = 9;
+let b = 78;
+console.log(a + b);
+const acc_id = 9696;
+let acc_email = "sarthakgupta81122@gmail.com";
+acc_city = "bhopal";
+console.log(acc_city);
+console.log(acc_id);
+//acc_id=45;
+console.log(acc_id);
+/*
+prefer not to use var.
+beacause of issue in block scope and functional scope.
+*/
+//console.table use for print all variable in the form of index and its corresponding values.
+console.table([acc_email, acc_id, a, b]);
+console.log("sarthak");
+console.log("poonam");
+console.table([acc_email, acc_city, acc_id]);
+
+("use strict"); // treat all js code as newer version
+
+alert(3 + 3); // we are useing node js not browser that's it will not run
+
+console.log(3 + 3);
+console.log();
