1de3b9b62c57a16e65ff506c74141c8eb055c63b
Sarthak Gupta <118627778+sarthakgupta096@users.noreply.github.com>
2024-01-16 07:22:31 +0000

    Datatypes

---
+++ basic/datatypes.js
@@ -0,0 +1,18 @@
+let a=5;
+let name="string";
+// symbol==>use for  unique
+//null ==>standalone value
+
+//object
+
+  //*** what is null?
+  //ans:null is a object.
+
+
+console.log(typeof a);
+console.log(typeof name);
+
+console.table([a,name])
+console.log(typeof null);
+console.log(typeof undefined)
+
